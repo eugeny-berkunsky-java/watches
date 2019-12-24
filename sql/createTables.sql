4c606a9d1090b9779638ae7feba9777e84ae9893
JayGrey <k_lite@ukr.net>
2019-12-24 22:12:36 +0200

    added scripts

--- sql/createTables.sql
+++ sql/createTables.sql
@@ -1,12 +1,22 @@
-
 set search_path to public;
 
-DROP TABLE if exists public."Country";
-
-CREATE TABLE public."Country"
+-----
+drop table if exists public."Country";
+create table public."Country"
 (
-    id serial not null primary key,
+    id   serial  not null primary key,
     name varchar not null unique
 );
+ALTER TABLE public."Country"
+    OWNER to watches;
 
-ALTER TABLE public."Country" OWNER to watches;
+-----
+drop table if exists public."Vendor";
+create table public."Vendor"
+(
+    id         serial  not null primary key,
+    vendorName varchar not null unique,
+    countryId  integer not null references public."Country" (id)
+);
+alter table public."Vendor"
+    owner to watches;
--- sql/populate.sql
+++ sql/populate.sql
@@ -1,5 +1,19 @@
-INSERT INTO public."Country" (name)
-VALUES ('Italy'),
-       ('France'),
-       ('UK'),
-       ('China');
+truncate public."Country";
+
+INSERT INTO public."Country" (id, name)
+VALUES (1, 'Italy'),
+       (2, 'France'),
+       (3, 'UK'),
+       (4, 'China');
+
+alter sequence public."Country_id_seq" restart with 5;
+
+----
+truncate public."Vendor";
+insert into public."Vendor" (id, vendorname, countryid)
+values (1, 'Cartier', 1),
+       (2, 'Bell & Ross', 2),
+       (3, 'Bremont', 3),
+       (4, 'Beijing Watch Factory', 4);
+
+alter sequence public."Vendor_id_seq" restart with 5;
