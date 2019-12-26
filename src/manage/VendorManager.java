d79fe2e49dc8aab4a78574b656bad1202d9b31c7
JayGrey <k_lite@ukr.net>
2019-12-26 09:07:17 +0200

    VendorDAO and VendorManager

--- src/manage/VendorManager.java
+++ src/manage/VendorManager.java
@@ -5,25 +5,41 @@ import model.DAO;
 import model.DAOFactory;
 import model.Vendor;
 
+import java.sql.SQLException;
+import java.util.Collections;
 import java.util.List;
 
 public class VendorManager {
     private DAO<Vendor> dao;
+    private Vendor emptyVendor;
 
     public VendorManager() {
         dao = DAOFactory.getVendorsDAO();
+        emptyVendor = new Vendor(-1, "<empty>", new Country(-1, "<empty>"));
     }
 
     public List<Vendor> getAll() {
-        return dao.getAll();
+        try {
+            return dao.getAll();
+        } catch (SQLException e) {
+            e.printStackTrace(System.err);
+        }
+
+        return Collections.emptyList();
     }
 
     public Vendor addVendor(String vendorName, int countryId) {
         if (vendorName == null || vendorName.trim().length() == 0) {
-            return null;
+            return emptyVendor;
         }
 
-        return dao.create(new Vendor(-1, vendorName, new Country(countryId, null)));
+        try {
+            return dao.create(new Vendor(-1, vendorName, new Country(countryId, null)));
+        } catch (SQLException e) {
+            e.printStackTrace(System.err);
+        }
+
+        return emptyVendor;
     }
 
     public boolean updateVendor(Vendor vendor) {
@@ -33,11 +49,23 @@ public class VendorManager {
                 || vendor.getCountry() == null) {
             return false;
         }
-        return dao.update(vendor);
+        try {
+            return dao.update(vendor);
+        } catch (SQLException e) {
+            e.printStackTrace(System.err);
+        }
+
+        return false;
     }
 
     public boolean deleteVendor(int vendorId) {
-        return dao.delete(vendorId);
+        try {
+            return dao.delete(vendorId);
+        } catch (SQLException e) {
+            e.printStackTrace(System.err);
+        }
+
+        return false;
     }
 }
 
--- src/model/VendorDAO.java
+++ src/model/VendorDAO.java
@@ -1,48 +1,35 @@
 package model;
 
-import main.Settings;
-
-import java.sql.*;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Properties;
+
+import static main.Settings.getConnection;
 
 class VendorDAO implements DAO<Vendor> {
 
-    private final String url;
-    private final Properties properties;
-
-    public VendorDAO() {
-        url = Settings.getConnectionURL();
-        properties = Settings.getConnectionProperties();
-    }
-
     @Override
-    public Vendor create(Vendor model) {
+    public Vendor create(Vendor model) throws SQLException {
 
         final String sql = "insert into public.\"Vendor\" (vendorname, countryid) " +
                 "VALUES (?, ?) returning *";
 
-        int savedId = -1;
-
-        try (final Connection conn = DriverManager.getConnection(url, properties)) {
-            final PreparedStatement st = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+        try (final PreparedStatement st
+                     = getConnection().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
             st.setString(1, model.getVendorName());
             st.setInt(2, model.getCountry().getId());
             st.execute();
             final ResultSet rs = st.getGeneratedKeys();
-            if (rs.next()) {
-                savedId = rs.getInt("id");
-            }
-        } catch (SQLException e) {
-            e.printStackTrace(System.err);
-        }
 
-        return getById(savedId);
+            return getById(rs.getInt("id"));
+        }
     }
 
     @Override
-    public List<Vendor> getAll() {
+    public List<Vendor> getAll() throws SQLException {
         final String sql =
                 "" +
                         "select " +
@@ -53,10 +40,10 @@ class VendorDAO implements DAO<Vendor> {
                         "from \"Vendor\"   as vendor " +
                         "    inner join \"Country\" as country on vendor.countryid = country.id";
 
-        List<Vendor> vendors = new ArrayList<>();
-        try (final Connection conn = DriverManager.getConnection(url, properties)) {
-            final PreparedStatement st = conn.prepareStatement(sql);
+        try (final PreparedStatement st = getConnection().prepareStatement(sql)) {
             final ResultSet rs = st.executeQuery();
+
+            List<Vendor> vendors = new ArrayList<>();
             while (rs.next()) {
                 int vendorId = rs.getInt("vendor_id");
                 String vendorName = rs.getString("vendor_vendorname");
@@ -64,14 +51,11 @@ class VendorDAO implements DAO<Vendor> {
                 String countryName = rs.getString("country_name");
                 vendors.add(new Vendor(vendorId, vendorName, new Country(countryId, countryName)));
             }
-        } catch (SQLException e) {
-            e.printStackTrace(System.err);
+            return vendors;
         }
-
-        return vendors;
     }
 
-    public Vendor getById(int id) {
+    public Vendor getById(int id) throws SQLException {
         final String sql =
                 "" +
                         "select " +
@@ -83,60 +67,42 @@ class VendorDAO implements DAO<Vendor> {
                         "    inner join \"Country\" as country on vendor.countryid = country.id " +
                         "where vendor.id = ?";
 
-        try (final Connection conn = DriverManager.getConnection(url, properties)) {
-            final PreparedStatement st = conn.prepareStatement(sql);
+        try (final PreparedStatement st = getConnection().prepareStatement(sql)) {
             st.setInt(1, id);
+
             final ResultSet rs = st.executeQuery();
-            if (rs.next()) {
-                final int vendorId = rs.getInt("vendor_id");
-                final String vendorName = rs.getString("vendor_vendorname");
-                final int countryId = rs.getInt("country_id");
-                final String countryName = rs.getString("country_name");
+            final int vendorId = rs.getInt("vendor_id");
+            final String vendorName = rs.getString("vendor_vendorname");
+            final int countryId = rs.getInt("country_id");
+            final String countryName = rs.getString("country_name");
 
-                return new Vendor(vendorId, vendorName, new Country(countryId, countryName));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace(System.err);
+            return new Vendor(vendorId, vendorName, new Country(countryId, countryName));
         }
-
-        return null;
     }
 
     @Override
-    public boolean update(Vendor model) {
+    public boolean update(Vendor model) throws SQLException {
         final String sql = "" +
                 "update public.\"Vendor\" " +
                 "set vendorname = ?, countryid = ? " +
                 "where id = ?;";
 
-        try (final Connection conn = DriverManager.getConnection(url, properties)) {
-            final PreparedStatement st = conn.prepareStatement(sql);
+        try (final PreparedStatement st = getConnection().prepareStatement(sql)) {
             st.setString(1, model.getVendorName());
             st.setInt(2, model.getCountry().getId());
             st.setInt(3, model.getId());
-            final int result = st.executeUpdate();
 
-            return result > 0;
-        } catch (SQLException e) {
-            e.printStackTrace(System.err);
+            return st.executeUpdate() > 0;
         }
-
-        return false;
     }
 
     @Override
-    public boolean delete(int id) {
+    public boolean delete(int id) throws SQLException {
         final String sql = "delete from public.\"Vendor\" where id = ?;";
 
-        try (final Connection conn = DriverManager.getConnection(url, properties)) {
-            final PreparedStatement st = conn.prepareStatement(sql);
+        try (final PreparedStatement st = getConnection().prepareStatement(sql)) {
             st.setInt(1, id);
-            final int result = st.executeUpdate();
-            return result > 0;
-        } catch (SQLException e) {
-            e.printStackTrace(System.err);
+            return st.executeUpdate() > 0;
         }
-        return false;
     }
-
 }
