9a563fce0493d086efa279aacc1a9f8c6b42ea2e
JayGrey <k_lite@ukr.net>
2019-12-29 17:12:51 +0200

    move Customer model update logic to DB

---
+++ sql/createTriggers.sql
@@ -0,0 +1,57 @@
+drop trigger if exists trigger_update_customer_model on public."CustomerModel";
+drop function if exists public.update_customer_model;
+
+create or replace function public.update_customer_model() returns trigger as
+$$
+declare
+    result_row record;
+begin
+    if tg_op = 'DELETE' then
+        delete from public."Customer" where id = old.customer_id;
+
+        if not found then
+            return null;
+        else
+            return old;
+        end if;
+
+    elsif tg_op = 'INSERT' then
+        if NEW.customer_sumoforders is null then
+            NEW.customer_sumoforders := 0.00;
+        end if;
+        if NEW.dcard_id is null then
+            NEW.dcard_id := 0;
+        end if;
+
+        insert into public."Customer" (name, sumoforders, discountcard_id)
+        values (NEW.customer_name, NEW.customer_sumoforders, NEW.dcard_id)
+        returning id into NEW.customer_id;
+
+        select * into result_row from public."CustomerModel" where customer_id = NEW.customer_id;
+
+        return result_row;
+
+    elsif tg_op = 'UPDATE' then
+        update public."Customer"
+        set name            = new.customer_name,
+            sumoforders     = new.customer_sumoforders,
+            discountcard_id = new.dcard_id
+        where id = new.customer_id;
+
+        if not found then
+            return null;
+        else
+            return new;
+        end if;
+
+    end if;
+
+
+end;
+$$ language plpgsql;
+
+create trigger trigger_update_customer_model
+    instead of insert or update or delete
+    on public."CustomerModel"
+    for each row
+execute function public.update_customer_model();
--- src/model/CustomerDAO.java
+++ src/model/CustomerDAO.java
@@ -13,9 +13,23 @@ import static main.Settings.getConnection;
 
 public class CustomerDAO implements DAO<Customer> {
 
+    static Customer createFromResultSet(ResultSet rs) {
+        try {
+            final int cId = rs.getInt("customer_id");
+            final String cName = rs.getString("customer_name");
+            final BigDecimal cSumOfOrders = rs.getBigDecimal("customer_sumoforders");
+
+            return new Customer(cId, cName, cSumOfOrders,
+                    DiscountCardDAO.createFromResultSet(rs));
+        } catch (SQLException e) {
+            throw new DBException(e);
+        }
+    }
+
     @Override
     public Customer create(Customer model) throws SQLException {
-        final String sql = "insert into public.\"Customer\" (name, sumoforders, discountcard_id) " +
+        final String sql = "insert into public.\"CustomerModel\" " +
+                "(customer_name, customer_sumoforders, dcard_id) " +
                 " values (?, ?, ?) returning *;";
 
         try (final PreparedStatement st = getConnection().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
@@ -27,51 +41,35 @@ public class CustomerDAO implements DAO<Customer> {
             final ResultSet rs = st.getGeneratedKeys();
             rs.next();
 
-            return getById(rs.getInt("id"));
+            return createFromResultSet(rs);
         }
     }
 
     @Override
     public List<Customer> getAll() throws SQLException {
         final String sql = "" +
-                "select c.id          as customer_id, " +
-                "       c.name        as customer_name, " +
-                "       c.sumoforders as customer_sumoforders, " +
-                "       dc.id         as customer_dc_id, " +
-                "       dc.number     as customer_dc_number, " +
-                "       dc.percent    as customer_dc_percent " +
-                "from public.\"Customer\" as c " +
-                "         inner join public.\"DiscountCard\" dc on c.discountcard_id = dc.id;";
+                "select * from public.\"CustomerModel\";";
 
 
         try (final PreparedStatement st = getConnection().prepareStatement(sql)) {
-            return executeAndReturnCollection(st, this::createCustomerFromResultSet);
+            return executeAndReturnCollection(st, CustomerDAO::createFromResultSet);
         }
     }
 
     @Override
     public Customer getById(int id) throws SQLException {
-        final String sql = "" +
-                "select c.id          as customer_id, " +
-                "       c.name        as customer_name, " +
-                "       c.sumoforders as customer_sumoforders, " +
-                "       dc.id         as customer_dc_id, " +
-                "       dc.number     as customer_dc_number, " +
-                "       dc.percent    as customer_dc_percent " +
-                "from public.\"Customer\" as c " +
-                "         inner join public.\"DiscountCard\" dc on c.discountcard_id = dc.id " +
-                "where c.id = ?; ";
+        final String sql = "select * from public.\"CustomerModel\" where customer_id = ?;";
 
         try (final PreparedStatement st = getConnection().prepareStatement(sql)) {
             st.setInt(1, id);
-            return executeAndReturnObject(st, this::createCustomerFromResultSet);
+            return executeAndReturnObject(st, CustomerDAO::createFromResultSet);
         }
     }
 
     @Override
     public boolean update(Customer model) throws SQLException {
-        final String sql = "update public.\"Customer\" set name = ?, sumoforders=?, " +
-                "discountcard_id = ? where id = ?;";
+        final String sql = "update public.\"CustomerModel\" set customer_name = ?, customer_sumoforders=?, " +
+                "dcard_id = ? where customer_id = ?;";
 
         try (final PreparedStatement st = getConnection().prepareStatement(sql)) {
             st.setString(1, model.getName());
@@ -84,27 +82,11 @@ public class CustomerDAO implements DAO<Customer> {
 
     @Override
     public boolean delete(int id) throws SQLException {
-        final String sql = "delete from public.\"Customer\" where id = ?;";
+        final String sql = "delete from public.\"CustomerModel\" where customer_id = ?;";
 
         try (final PreparedStatement st = getConnection().prepareStatement(sql)) {
             st.setInt(1, id);
             return st.executeUpdate() > 0;
-        }
-    }
-
-    private Customer createCustomerFromResultSet(ResultSet rs) {
-        try {
-            final int cId = rs.getInt("customer_id");
-            final String cName = rs.getString("customer_name");
-            final BigDecimal cSumOfOrders = rs.getBigDecimal("customer_sumoforders");
-            final int dcId = rs.getInt("customer_dc_id");
-            final String dcNumber = rs.getString("customer_dc_number");
-            final BigDecimal dcPercent = rs.getBigDecimal("customer_dc_percent");
-
-            return new Customer(cId, cName, cSumOfOrders,
-                    new DiscountCard(dcId, dcNumber, dcPercent));
-        } catch (SQLException e) {
-            throw new DBException(e);
         }
     }
 }
