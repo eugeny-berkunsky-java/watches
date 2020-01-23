0a978446e49e1366e7a4a16e6ed916fdcebd0751
JayGrey <k_lite@ukr.net>
2020-01-23 21:28:20 +0200

    added countryService

--- src/main/webapp/lib/countries.js
+++ src/main/webapp/lib/countries.js
@@ -4,170 +4,140 @@ const nameField = document.getElementById("country-dialog-name");
 const deleteNameField = document.getElementById("country-dialog-delete-name");
 
 function redrawCountriesPlane() {
-    storage.selectedCountry = -1;
-    document.getElementById("country-edit-button").classList.add("disabled");
-    document.getElementById("country-delete-button").classList.add("disabled");
+  storage.selectedCountry = -1;
+  document.getElementById("country-edit-button").classList.add("disabled");
+  document.getElementById("country-delete-button").classList.add("disabled");
 
-    //
-    const table = document.getElementById("countries-table");
+  //
+  const table = document.getElementById("countries-table");
 
-    if (storage.countries.length > 0) {
+  if (storage.countries.length > 0) {
+    buildCountryTable(table, storage.countries);
+  } else {
+    countryService.getAll()
+      .then(countries => {
+        storage.countries = countries;
+        storage.countries.sort((a, b) => a.id - b.id);
         buildCountryTable(table, storage.countries);
-    } else {
-        loadDataFromServer('country')
-            .then(countries => {
-                storage.countries = countries;
-                storage.countries.sort((a, b) => a.id - b.id);
-                buildCountryTable(table, storage.countries);
-            });
-    }
+      })
+  }
 }
 
 function buildCountryTable(rootElement, data) {
 
-    function selectTableRow() {
-        if (storage.selectedCountry !== -1) {
-            const previousRow = this.parentElement.querySelector(`tr[data-country-id="${storage.selectedCountry}"]`);
-            if (previousRow !== null) {
-                previousRow.classList.remove("table-info");
-            }
-        }
-
-        document.getElementById("country-edit-button").classList.remove("disabled");
-        document.getElementById("country-delete-button").classList.remove("disabled");
-
-        storage.selectedCountry = Number.parseInt(this.getAttribute("data-country-id"));
-        this.classList.add("table-info");
+  function selectTableRow() {
+    if (storage.selectedCountry !== -1) {
+      const previousRow = this.parentElement.querySelector(`tr[data-country-id="${storage.selectedCountry}"]`);
+      if (previousRow !== null) {
+        previousRow.classList.remove("table-info");
+      }
     }
 
-    const tbody = rootElement.querySelector("tbody");
-    removeChildElements(tbody);
+    document.getElementById("country-edit-button").classList.remove("disabled");
+    document.getElementById("country-delete-button").classList.remove("disabled");
 
-    data.forEach(country => {
+    storage.selectedCountry = Number.parseInt(this.getAttribute("data-country-id"));
+    this.classList.add("table-info");
+  }
 
-        const tr = createElement("tr", {"data-country-id": country.id},
-            createElement("td", {}, createTextElement(country.id)),
-            createElement("td", {}, createTextElement(country.name)),
-        );
+  const tbody = rootElement.querySelector("tbody");
+  removeChildElements(tbody);
 
-        tr.addEventListener('click', selectTableRow);
-        tbody.appendChild(tr);
-    });
+  data.forEach(country => {
+
+    const tr = createElement("tr", {"data-country-id": country.id},
+      createElement("td", {}, createTextElement(country.id)),
+      createElement("td", {}, createTextElement(country.name)),
+    );
+
+    tr.addEventListener('click', selectTableRow);
+    tbody.appendChild(tr);
+  });
 }
 
 function showUpdateCountryDialog() {
-    function processUpdate(data) {
+  function processUpdate(data) {
+    countryService.update(data)
+      .then(country => {
+          storage.countries = storage.countries.map(c => c.id === country.id ? country : c);
+          redrawCountriesPlane();
+        }
+      )
+      .catch(error => console.error(error));
+  }
 
-        const endpoint = `http://localhost:8080/api/country/${data.id}`;
-        fetch(endpoint, {
-            method: 'PUT',
-            headers: {'Content-type': 'application/json'},
-            body: JSON.stringify(data)
-        })
-            .then(response => {
-                if (response.ok) {
-                    return response.json();
-                } else {
-                    throw new Error();
-                }
-            })
-            .then(country => {
-                storage.countries = storage.countries.map(c => {
-                    return c.id === country.id ? country : c;
-                });
-                redrawCountriesPlane();
-            })
-            .catch(error => console.error(error));
-    }
 
-    if (storage.selectedCountry !== -1) {
-        const country = storage.countries.find(c => c.id === storage.selectedCountry);
-        showCountryDialog("Update Country", country, "Update", processUpdate);
-    }
+  if (storage.selectedCountry !== -1) {
+    const country = storage.countries.find(c => c.id === storage.selectedCountry);
+    showCountryDialog("Update Country", country, "Update", processUpdate);
+  }
 }
 
 function showCreateCountryDialog() {
-    function processCreate(data) {
+  function processCreate(data) {
+    countryService.create(data)
+      .then(country => {
+        storage.countries = [...storage.countries, country];
+        redrawCountriesPlane();
+      })
+      .catch(console.error);
+  }
 
-        const endpoint = 'http://localhost:8080/api/country/';
-        fetch(endpoint, {
-            method: 'POST',
-            headers: {'Content-type': 'application-json'},
-            body: JSON.stringify(data)
-        })
-            .then(response => {
-                if (response.ok) {
-                    return response.json();
-                } else {
-                    throw new Error();
-                }
-            })
-            .then(country => {
-                storage.countries = [...storage.countries, country];
-                redrawCountriesPlane();
-            })
-            .catch(error => console.error(error))
-
-    }
-
-    showCountryDialog("Add new Country", null, "Create", processCreate);
+  showCountryDialog("Add new Country", null, "Create", processCreate);
 }
 
 function showDeleteCountryDialog() {
-    if (storage.selectedCountry !== -1) {
-        const country = storage.countries.find(country => country.id === storage.selectedCountry);
-        if (country) {
-            deleteNameField.value = country.name;
-        }
-
-        deleteCountryDialog.style.display = 'block';
-
+  if (storage.selectedCountry !== -1) {
+    const country = storage.countries.find(country => country.id === storage.selectedCountry);
+    if (country) {
+      deleteNameField.value = country.name;
     }
+
+    deleteCountryDialog.style.display = 'block';
+
+  }
 }
 
 function cancelDeleteCountryHandler() {
-    deleteCountryDialog.style.display = 'none';
+  deleteCountryDialog.style.display = 'none';
 }
 
 function proceedDeleteCountryHandler() {
-    const endpoint = `http://localhost:8080/api/country/${storage.selectedCountry}`;
-    fetch(endpoint, {
-        method: 'DELETE'
+  countryService.delete(storage.selectedCountry)
+    .then(() => {
+      const removedId = storage.selectedCountry;
+      storage.countries = storage.countries.filter(c => c.id !== removedId);
+      redrawCountriesPlane();
     })
-        .then(response => {
-            if (response.ok) {
-                const removedId = storage.selectedCountry;
-                storage.countries = storage.countries.filter(c => c.id !== removedId);
-                redrawCountriesPlane();
-            }
-        });
-
-    deleteCountryDialog.style.display = 'none';
+    .catch(console.error)
+    .finally(() => {
+      deleteCountryDialog.style.display = 'none'
+    });
 }
 
 function showCountryDialog(title, model, buttonTitle, processFunction) {
-    const buttonHandler = () => {
-        processFunction({name: nameField.value, id: storage.selectedCountry});
-        closeCountryDialog();
-        dialogButtonTitle.removeEventListener('click', buttonHandler);
-    };
+  const buttonHandler = () => {
+    processFunction({name: nameField.value, id: storage.selectedCountry});
+    closeCountryDialog();
+    dialogButtonTitle.removeEventListener('click', buttonHandler);
+  };
 
-    const dialogTitle = document.getElementById("country-dialog-title");
-    dialogTitle.innerText = title;
+  const dialogTitle = document.getElementById("country-dialog-title");
+  dialogTitle.innerText = title;
 
-    const dialogButtonTitle = document.getElementById("country-dialog-button-title");
-    dialogButtonTitle.innerText = buttonTitle;
-    dialogButtonTitle.addEventListener('click', buttonHandler);
+  const dialogButtonTitle = document.getElementById("country-dialog-button-title");
+  dialogButtonTitle.innerText = buttonTitle;
+  dialogButtonTitle.addEventListener('click', buttonHandler);
 
-    if (model) {
-        nameField.value = model.name;
-    } else {
-        nameField.value = '';
-    }
+  if (model) {
+    nameField.value = model.name;
+  } else {
+    nameField.value = '';
+  }
 
-    updateCountryDialog.style.display = 'block';
+  updateCountryDialog.style.display = 'block';
 }
 
 function closeCountryDialog() {
-    updateCountryDialog.style.display = 'none';
+  updateCountryDialog.style.display = 'none';
 }
--- src/main/webapp/lib/utils.js
+++ src/main/webapp/lib/utils.js
@@ -1,46 +1,112 @@
 function removeChildElements(rootElement) {
-    let child = rootElement.lastElementChild;
-    while (child) {
-        rootElement.removeChild(child);
-        child = rootElement.lastElementChild;
-    }
+  let child = rootElement.lastElementChild;
+  while (child) {
+    rootElement.removeChild(child);
+    child = rootElement.lastElementChild;
+  }
 }
 
 function createElement(tagName, attributes, ...children) {
-    const element = document.createElement(tagName);
-    Object.keys(attributes).forEach(key => {
-        element.setAttribute(key, attributes[key]);
-    });
+  const element = document.createElement(tagName);
+  Object.keys(attributes).forEach(key => {
+    element.setAttribute(key, attributes[key]);
+  });
 
-    children.forEach(child => element.appendChild(child));
+  children.forEach(child => element.appendChild(child));
 
-    return element;
+  return element;
 }
 
 function createTextElement(data) {
-    return document.createTextNode(data);
+  return document.createTextNode(data);
 }
 
 function formatDate(isoString) {
-    const date = new Date(isoString);
-    const day = ("0" + date.getDate()).slice(-2);
-    const month = ("0" + (date.getMonth() + 1)).slice(-2);
-    const year = date.getFullYear();
+  const date = new Date(isoString);
+  const day = ("0" + date.getDate()).slice(-2);
+  const month = ("0" + (date.getMonth() + 1)).slice(-2);
+  const year = date.getFullYear();
 
-    const hours = ("0" + date.getHours()).slice(-2);
-    const minutes = ("0" + date.getMinutes()).slice(-2);
-    const seconds = ("0" + date.getSeconds()).slice(-2);
+  const hours = ("0" + date.getHours()).slice(-2);
+  const minutes = ("0" + date.getMinutes()).slice(-2);
+  const seconds = ("0" + date.getSeconds()).slice(-2);
 
-    return `${day}.${month}.${year} ${hours}:${minutes}:${seconds}`;
+  return `${day}.${month}.${year} ${hours}:${minutes}:${seconds}`;
 }
 
 function loadDataFromServer(data) {
-    const endpoint = `http://localhost:8080/api/${data}/`;
+  const endpoint = `http://localhost:8080/api/${data}/`;
 
-    return new Promise((resolve, reject) => {
-        fetch(endpoint)
-            .then(response => response.json())
-            .then(resolve)
-            .catch(reject);
-    });
+  return new Promise((resolve, reject) => {
+    fetch(endpoint)
+      .then(response => response.json())
+      .then(resolve)
+      .catch(reject);
+  });
 }
+
+const countryService = {
+  getAll: () => new Promise((resolve, reject) => {
+    const endpoint = "http://localhost:8080/api/country/";
+
+    fetch(endpoint)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          return Promise.reject(response.json());
+        }
+      })
+      .then(resolve)
+      .catch(promise => promise.then(json => reject(json.message)));
+  }),
+
+  getById: id => new Promise((resolve, reject) => {
+    const endpoint = `http://localhost:8080/api/country/${id}`;
+
+    fetch(endpoint)
+      .then(response => response.ok
+        ? response.json()
+        : Promise.reject(response.json()))
+      .then(resolve)
+      .catch(promise => promise.then(json => reject(json.message)));
+  }),
+
+  update: data => new Promise((resolve, reject) => {
+    const endpoint = `http://localhost:8080/api/country/${data.id}`;
+    fetch(endpoint, {
+      method: 'PUT',
+      headers: {'Content-type': 'application/json'},
+      body: JSON.stringify(data)
+    })
+      .then(response =>
+        response.ok ? response.json() : Promise.reject(response.json())
+      )
+      .then(resolve)
+      .catch(promise => promise.then(json => reject(json.message)));
+  }),
+
+  create: data => new Promise((resolve, reject) => {
+    const endpoint = 'http://localhost:8080/api/country/';
+    fetch(endpoint, {
+      method: 'POST',
+      headers: {'Content-type': 'application-json'},
+      body: JSON.stringify(data)
+    })
+      .then(response => response.ok
+        ? response.json()
+        : Promise.reject(response.json()))
+      .then(resolve)
+      .catch(promise => promise.then(json => reject(json.message)))
+  }),
+
+  delete: id => new Promise((resolve, reject) => {
+    const endpoint = `http://localhost:8080/api/country/${id}`;
+    fetch(endpoint, {
+      method: 'DELETE'
+    })
+      .then(response => response.ok ? Promise.resolve() : Promise.reject(response.json()))
+      .then(resolve)
+      .catch(promise => promise.then(json => reject(json.message)))
+  }),
+};
